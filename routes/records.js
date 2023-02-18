db49e811e4a9dec3f1d69b909bb9716bb0824061
THSPSS <111225654+THSPSS@users.noreply.github.com>
2023-02-18 11:40:16 +0900

    Update records.js
    record route update

--- routes/records.js
+++ routes/records.js
@@ -3,8 +3,12 @@ import { getRecords , getRecord , addRecord } from "../controllers/record.js";
 
 const router = express.Router();
 
+
 router.get("/", getRecords);
 router.get("/:member_no", getRecord);
+router.get("/count/:member_no", getRecordCount);
+router.get("/stamp/:member_no", getStampRecord);
 router.post("/", addRecord);
+
 
 export default router;
