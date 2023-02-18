9e756a26d634c9af10ba935831d5aafe6ef83f94
THSPSS <111225654+THSPSS@users.noreply.github.com>
2023-02-18 11:34:08 +0900

    Update location.js
    get location count

--- controllers/location.js
+++ controllers/location.js
@@ -10,9 +10,16 @@ export const getLocations = (req,res)=>{
    })
 }
 
-export const getLocation = (req,res)=>{
-    res.json("from controller")
-}
+export const getLocationsCount = (req,res)=>{
+    const q = "SELECT COUNT(*) AS total FROM location";
+ 
+    db.query(q, (err,data)=>{
+     if(err) return res.send(err)
+ 
+     return res.status(200).json(data);
+    })
+ }
+
 
 export const addLocation = (req,res)=>{
     res.json("from controller")
