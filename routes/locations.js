ee30867bc3dfc16c4987a61ce36d49fbce9bf0b1
THSPSS <111225654+THSPSS@users.noreply.github.com>
2023-02-18 11:47:09 +0900

    Update locations.js
    delete get one location route and module

--- routes/locations.js
+++ routes/locations.js
@@ -1,11 +1,10 @@
 import express from "express"
-import { getLocations, getLocation , addLocation, deleteLocation , updateLocation } from "../controllers/location.js"
+import { getLocations, addLocation, deleteLocation , updateLocation } from "../controllers/location.js"
 
 const router = express.Router()
 
 router.get("/", getLocations)
 router.get("/count", getLocationsCount)
-router.get("/:id", getLocation)
 router.post("/", addLocation)
 router.delete("/:id", deleteLocation)
 router.put("/:id", updateLocation)
